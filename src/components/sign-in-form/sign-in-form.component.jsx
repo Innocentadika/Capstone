67022a83c0be67222a76630d41d693415f77cd18
innocentadika <innocentadika18@gmail.com>
2023-10-28 13:40:23 +0300

    finishing of the sign in form

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -35,6 +35,18 @@ const SignInForm = () => {
         console.log(response); 
         resetFormFields();
       } catch (error) {
+        switch (error.code) {
+          case 'auth/wrong-password':
+            alert('incorrect password or email');
+            break;
+        
+          case 'auth/user-not-found':
+          alert('no user associated with this email, please sign up to continue');
+          break;
+
+            default:
+              console.log(error);
+        }
         
       }
     };
